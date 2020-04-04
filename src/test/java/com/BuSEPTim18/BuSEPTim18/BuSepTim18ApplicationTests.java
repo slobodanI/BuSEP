0d1c94402b5786ca6f28afb0129f5eed213f5674
slobodan <slobodansrk@gmail.com>
2020-04-04 14:56:01 +0200

    napokon

---
+++ src/test/java/com/BuSEPTim18/BuSEPTim18/BuSepTim18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BuSEPTim18.BuSEPTim18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuSepTim18ApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
