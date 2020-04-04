157f12b6ea93fe431164f612b4b3cc0a13efd578
slobodan <slobodansrk@gmail.com>
2020-04-04 14:39:38 +0200

    kreiran projekat

---
+++ BuSEPTim18/src/main/java/com/BuSEPTim18/BuSEPTim18/BuSepTim18Application.java
@@ -0,0 +1,13 @@
+package com.BuSEPTim18.BuSEPTim18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuSepTim18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuSepTim18Application.class, args);
+	}
+
+}
